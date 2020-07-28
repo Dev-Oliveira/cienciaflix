d8079b74e99e30d81db13d874869985dca03e6c6
Hellom Oliveira <hellom-mendes@hotmail.com>
2020-07-28 01:52:03 +0100

    Setup Inicial do projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Menu from './componentes/menu'
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain from './componentes/BannerMain';
+import Carousel from './componentes/Carousel';
+import Footer from './componentes/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ background: "#141414" }}>
+      <Menu />
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O espaço-tempo é um dos conceitos físicos mais famosos. Ele surge como uma consequência da relatividade de Einstein e muda drasticamente a maneira como nós enxergamos o universo. Explicar o conceito não é fácil, mas aqui vai a minha tentativa nesse novo vídeo da série Explicado. Espero que gostem!"}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[3]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[4]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[5]}
+      />      
+
+      <Footer />
     </div>
   );
 }
