bad3229b6158a9114626f161c8d27ab59c2f7490
Hellom Oliveira <hellom-mendes@hotmail.com>
2020-07-30 00:55:54 +0100

    cadastro

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,26 +1,98 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PageDefault from '../../../componentes/PageDefault';
 import { Link } from 'react-router-dom';
+import FormField from '../../../componentes/FormField';
 
 
 function CadastroCategoria(){
-    return(
+  const valoresInicias = {
+    nome:'',
+    descricao:'',
+    cor:'#000'
+  }
+  const [categorias, setCategorias] = useState([])
+  const [values, setValues] = useState(valoresInicias);
+  
+  
+
+  function setValue(chave, valor){
+    setValues(
+      {
+        ...values,
+        [chave]: valor,
+      }
+    )
+  }
+
+  function hendleChange(infosDoEvento){
+    setValue( 
+      infosDoEvento.target.getAttribute('name'),
+      infosDoEvento.target.value,
+    )
+  }
+  
+  return(
       <PageDefault>
-        <h1>Cadastro de Categoria</h1>
+        <h1>Cadastro de Categoria: {values.nome}</h1>
 
-        <form>
+        <form onSubmit={function handleSubmit(infosDoEvento){
+          infosDoEvento.preventDefault();
+          console.log('VOcê tentou enviar o form')
+          setCategorias([
+            ...categorias,
+            values
+          ]);
+          
+          setValues(valoresInicias)
+        }}>
 
-        <label>
-            Nome da Categoria:
-            <input
+
+        
+          <FormField
+            label="Nome da Categoria"
             type="text"
-            />
-        </label>
+            name="nome"
+            value={values.nome}
+            onChange={hendleChange}
+          />
+      
+
+          <div>
+            <label>
+                Descrição:
+                <textarea
+                type="text"
+                value={values.descricao}
+                name="descricao"
+                onChange={hendleChange}
+                />
+            </label>
+          </div>
+
+          <FormField
+            label="Cor"
+            type="color"
+            name="cor"
+            value={values.cor}
+            onChange={hendleChange}
+          />
+        
 
         <button>
             Cadastrar
         </button>
         </form>
+
+        <ul>
+          {categorias.map((categoria, indice) => {
+            return (
+              <li key={`${categoria}${indice}`}>
+                {categoria.nome}
+              </li>
+            )
+          })}
+        </ul>
+
         <Link to="/">
             Ir para a Home
         </Link>
